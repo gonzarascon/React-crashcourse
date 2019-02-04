fda040358053a08dc545abac9bc6b535cbf5c4be
gitmaimo <rascon.gonzalo@maimonides.edu>
2019-02-04 13:01:09 -0300

    Added propTypes, refactored folder tree

---
+++ WeatherApp/weather-app/src/components/WeatherLocation/WeatherData/WeatherTemperature.js
@@ -0,0 +1,38 @@
+import React from "react";
+import WeatherIcons from "react-weathericons";
+import PropTypes from "prop-types";
+import {
+  CLOUD,
+  CLOUDY,
+  SUN,
+  RAIN,
+  SNOW,
+  WINDY
+} from "./../../../constants/weathers";
+
+const icons = {
+  [SUN]: "day-sunny",
+  [CLOUD]: "cloud",
+  [CLOUDY]: "cloudy",
+  [RAIN]: "rain",
+  [SNOW]: "snow",
+  [WINDY]: "windy"
+};
+const getWeatherIcon = weatherState => {
+  const icon = icons[weatherState];
+  if (icon) return <WeatherIcons name={icon} size="2x" />;
+  else return <WeatherIcons name={"day-sunny"} size="2x" />;
+};
+const WeatherTemperature = ({ temperature, weatherState }) => (
+  <div>
+    {getWeatherIcon(weatherState)}
+    <span>{`${temperature} C`}</span>
+  </div>
+);
+
+WeatherTemperature.propTypes = {
+  temperature: PropTypes.number.isRequired,
+  weatherState: PropTypes.string.isRequired
+};
+
+export default WeatherTemperature;
--- WeatherApp/weather-app/src/components/WeatherLocation/WeatherData/index.js
+++ WeatherApp/weather-app/src/components/WeatherLocation/WeatherData/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import WeatherExtraInfo from "./WeatherExtraInfo";
 import WeatherTemperature from "./WeatherTemperature";
+import { RAIN } from "./../../../constants/weathers";
 
 const WeatherData = () => (
   <div>
-    <WeatherTemperature />
+    <WeatherTemperature temperature={20} weatherState={RAIN} />
     <WeatherExtraInfo humidity={80} wind={"10 m/S"} />
   </div>
 );
