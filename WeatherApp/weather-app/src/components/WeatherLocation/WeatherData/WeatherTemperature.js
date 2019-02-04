af2e94f01f3a0cc669caf294da5cbb0dedabecd3
gitmaimo <rascon.gonzalo@maimonides.edu>
2019-02-04 13:29:28 -0300

    Added some styles

--- WeatherApp/weather-app/src/components/WeatherLocation/WeatherData/WeatherTemperature.js
+++ WeatherApp/weather-app/src/components/WeatherLocation/WeatherData/WeatherTemperature.js
@@ -1,6 +1,7 @@
 import React from "react";
 import WeatherIcons from "react-weathericons";
 import PropTypes from "prop-types";
+
 import {
   CLOUD,
   CLOUDY,
@@ -9,6 +10,7 @@ import {
   SNOW,
   WINDY
 } from "./../../../constants/weathers";
+import "./styles.css";
 
 const icons = {
   [SUN]: "day-sunny",
@@ -24,7 +26,7 @@ const getWeatherIcon = weatherState => {
   else return <WeatherIcons name={"day-sunny"} size="2x" />;
 };
 const WeatherTemperature = ({ temperature, weatherState }) => (
-  <div>
+  <div className="weatherTemperatureCont">
     {getWeatherIcon(weatherState)}
     <span>{`${temperature} C`}</span>
   </div>
--- WeatherApp/weather-app/src/components/WeatherLocation/WeatherData/index.js
+++ WeatherApp/weather-app/src/components/WeatherLocation/WeatherData/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import WeatherExtraInfo from "./WeatherExtraInfo";
 import WeatherTemperature from "./WeatherTemperature";
+
 import { RAIN } from "./../../../constants/weathers";
+import "./styles.css";
 
 const WeatherData = () => (
-  <div>
+  <div className="weatherDataCont">
     <WeatherTemperature temperature={20} weatherState={RAIN} />
     <WeatherExtraInfo humidity={80} wind={"10 m/S"} />
   </div>
