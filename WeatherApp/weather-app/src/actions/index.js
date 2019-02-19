b0db25ae421053c57b73d5a7a6ffcbd50eaa58ec
gonzarascon <gonzarascon@gmail.com>
2019-02-19 01:03:00 -0300

    added new action for future refactor

--- WeatherApp/weather-app/src/actions/index.js
+++ WeatherApp/weather-app/src/actions/index.js
@@ -1,3 +1,22 @@
+import getUrlForecastByCity from "./../services/getUrlForecastByCity";
+import transformForecast from "./../services/transformForecast";
+
 export const SET_CITY = "SET_CITY";
 
 export const setCity = payload => ({ type: SET_CITY, payload });
+
+export const fetchForecast = payload => {
+  return dispatch => {
+    const url_forecast = getUrlForecastByCity(dispatch);
+
+    //Activar en el estado un indicador de busqueda de datos
+    fetch(url_forecast)
+      .then(data => data.json())
+      .then(weather_data => {
+        const forecastData = transformForecast(weather_data);
+        console.log(forecastData);
+
+        // Modificar el estado con el resultado de la promise()
+      });
+  };
+};
--- WeatherApp/weather-app/src/store/index.js
+++ WeatherApp/weather-app/src/store/index.js
@@ -1,12 +1,15 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import { city } from "./../reducers/city";
 
 const initialState = {
   city: "Buenos Aires"
 };
 
+const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export const store = createStore(
   city,
   initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhacers(applyMiddleware(thunk))
 );
